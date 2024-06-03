7fc87f1c4c8bffea0cf4a89380a5e2670a807da8
Evan <evanboisdanghien@gmail.com>
2024-06-03 22:47:56 +0200

    piece totalement terminé : géré les déplacement et l'afffichage

--- src/main/java/com/s201_chess/HelloController.java
+++ src/main/java/com/s201_chess/HelloController.java
@@ -2,13 +2,26 @@ package com.s201_chess;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import java.net.URL;
+import java.util.Objects;
+import java.util.ResourceBundle;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private ImageView blackPP;
 
-    @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    public void initialize() {
+        System.out.println("Initialisation du controlleur..");
+        //welcomeText.setText("Welcome to JavaFX Application!");
+        URL blackPPUrl = getClass().getResource("/images/blackPP.png");
+        if (blackPPUrl != null) {
+            blackPP.setImage(new Image(blackPPUrl.toExternalForm()));
+        } else {
+            System.err.println("Resource not found: aa images/blackPP.png");
+            // Optionally, you can handle this case by setting a default image or taking other actions
+        }
     }
 }
