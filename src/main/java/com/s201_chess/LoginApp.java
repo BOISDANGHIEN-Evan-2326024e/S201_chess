eddd6d33ce0bb20a2d05e215b6870c21c02f07c5
mathieu <mathieu.gravoille@etu.univ-amu.fr>
2024-06-03 11:56:25 +0200

    tentative infructueuse de résoudre la "Location is not set." erreur

--- src/main/java/com/s201_chess/LoginApp.java
+++ src/main/java/com/s201_chess/LoginApp.java
@@ -6,6 +6,7 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
 
 public class LoginApp extends Application {
     public static void main(String[] args) {
@@ -15,7 +16,8 @@ public class LoginApp extends Application {
     @Override
     public void start(Stage stage) throws IOException {
 
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getClassLoader().getResource("com.s201_chess/LoginView.fxml"));
+        URL fxmlLocation = getClass().getClassLoader().getResource("com.s201_chess/LoginView.fxml");
+        FXMLLoader fxmlLoader = new FXMLLoader(fxmlLocation);
         Scene scene = new Scene(fxmlLoader.load(), 534, 360);
         stage.setScene(scene);
         stage.setResizable(false);
