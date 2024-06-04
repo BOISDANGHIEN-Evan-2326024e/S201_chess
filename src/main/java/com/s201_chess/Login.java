dd180671f431c3fe081157715c14aabfb8b0ba2c
mathieu <mathieu.gravoille@etu.univ-amu.fr>
2024-06-04 12:12:50 +0200

    Location set, et début création interface login chess.com like

--- src/main/java/com/s201_chess/Login.java
+++ src/main/java/com/s201_chess/Login.java
@@ -6,9 +6,8 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.net.URL;
 
-public class LoginApp extends Application {
+public class Login extends Application {
     public static void main(String[] args) {
         Application.launch(args);
     }
@@ -16,9 +15,9 @@ public class LoginApp extends Application {
     @Override
     public void start(Stage stage) throws IOException {
 
-        URL fxmlLocation = getClass().getClassLoader().getResource("com.s201_chess/LoginView.fxml");
-        FXMLLoader fxmlLoader = new FXMLLoader(fxmlLocation);
+        FXMLLoader fxmlLoader = new FXMLLoader(Login.class.getResource("Login.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 534, 360);
+        scene.getStylesheets().add(Login.class.getResource("Login.css").toExternalForm());
         stage.setScene(scene);
         stage.setResizable(false);
         stage.setTitle("chess.com");
