259ff0f8e4bb4cd8efbdaa4a904301c79366945e
BOISDANGHIEN-Evan-2326024e <keyzaks23@gmail.com>
2024-06-04 14:49:12 +0200

    Début du mouvement des pieces

--- src/main/java/com/s201_chess/HelloController.java
+++ src/main/java/com/s201_chess/HelloController.java
@@ -9,14 +9,17 @@ import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.Objects;
 import java.util.ResourceBundle;
 
 public class HelloController {
+    private Piece selectedPiece = null;
     @FXML
     private ImageView blackPP;
     @FXML
     private GridPane grid;
+
     public void initialize() {
         System.out.println("Initialisation du controlleur..");
         //welcomeText.setText("Welcome to JavaFX Application!");
@@ -27,12 +30,31 @@ public class HelloController {
             System.err.println("Resource not found: aa images/blackPP.png");
             // Optionally, you can handle this case by setting a default image or taking other actions
         }
-    Partie partietest = new Partie(new Joueur("Joueur1","a","a"), new Joueur("Joueur2","a","a"));
+        Partie partietest = new Partie(new Joueur("Joueur1","a","a"), new Joueur("Joueur2","a","a"));
         affichage_plateau(partietest);
+        grid.setOnMouseClicked(event -> {
+            System.out.println("coucou");
+            if (selectedPiece != null) {
+                // Calculez la nouvelle position en fonction de la position du clic
+                System.out.println("caca");
+                int newPositionH = (int) (event.getY() / 100);
+                int newPositionV = (int) (event.getX() / 100);
+                ArrayList<ArrayList<Integer>> mvt_possible=partietest.mvt_possible(selectedPiece);
+                // Déplacez la pièce dans le code
+                partietest.deplacer_piece(selectedPiece.getPosition_h(), selectedPiece.getPosition_v(), newPositionH, newPositionV,mvt_possible,selectedPiece);
+
+                // Déplacez la pièce dans l'interface graphique
+                grid.getChildren().remove(selectedPiece.getImage());
+                grid.add(selectedPiece.getImage(), newPositionV, newPositionH);
+
+                // Réinitialisez la pièce sélectionnée
+                selectedPiece = null;
+            }
+        });
     }
 
-    public void affichage_plateau(Partie partietest){
-        for (int i= 0; i < 8; i++) {
+    public void affichage_plateau(Partie partietest) {
+        for (int i = 0; i < 8; i++) {
             for (int j = 0; j < 8; j++) {
                 // Créer un rectangle pour représenter la case
                 Rectangle rect = new Rectangle(100, 100);
@@ -42,18 +64,23 @@ public class HelloController {
                 // Si une pièce d'échecs est présente à ces coordonnées
                 Piece piece = partietest.getPlateau().get(i).get(j);
                 if (piece != null) {
+
                     // Créer un ImageView avec l'image de la pièce
-                    ImageView image=piece.getImage();
+                    ImageView image = piece.getImage();
                     image.setFitHeight(100);
                     image.setFitWidth(100);
+                    // Ajoutez un gestionnaire d'événements à l'image
+                    image.setOnMouseClicked(event -> {
+                        System.out.println("yo");
+                        // Enregistrez la pièce sélectionnée et attendez le prochain clic
+                        // pour déplacer la pièce
+                        // Vous pouvez stocker la pièce sélectionnée dans une variable d'instance
+                        selectedPiece = piece;
+                    });
 
                     grid.add(image, j, i);
                 }
-
-                // Ajouter le rectangle à la cellule correspondante du GridPane
             }
         }
     }
-
-
 }
