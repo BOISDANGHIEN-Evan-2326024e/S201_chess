f8b545b426e60ce516e0a8dab51cb416546c7ab0
mathieu <mathieu.gravoille@etu.univ-amu.fr>
2024-06-05 16:41:51 +0200

    ajout des classes FileManager (pour enregistrer les utilisateurs dans un fichier...je crois) et User (class spécifique aux utilisateurs).

--- src/main/java/com/s201_chess/LoginController.java
+++ src/main/java/com/s201_chess/LoginController.java
@@ -6,23 +6,28 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
+
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
 import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.beans.EventHandler;
+import java.io.File;
 import java.io.IOException;
 
 public class LoginController extends GridPane {
     @FXML
     VBox vb;
     @FXML
-    Label username;
-    @FXML
-    Label password;
-    @FXML
     TextField usernameField;
+    @FXML
+    TextField firstNameField;
+    @FXML
+    TextField lastNameField;
     @FXML
     Button loginButton;
 
@@ -30,7 +35,7 @@ public class LoginController extends GridPane {
         try {
             FXMLLoader loader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
             Stage stage = (Stage) button.getScene().getWindow();
-            stage.setScene(new Scene(loader.load()));
+            stage.setScene(new Scene(loader.load(), 777, 659));
             stage.show();
         } catch (IOException e) {
             e.printStackTrace();
