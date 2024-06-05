b4c01a0c246ec1470c8b92251646b17904e71016
BOISDANGHIEN-Evan-2326024e <keyzaks23@gmail.com>
2024-06-05 09:51:12 +0200

    Mouvements completement fini avec tour par tour sur lesm ouvements, a faire echec, echec et mat et timer

--- src/main/java/com/s201_chess/HelloController.java
+++ src/main/java/com/s201_chess/HelloController.java
@@ -22,6 +22,7 @@ public class HelloController {
     private GridPane grid;
 
 
+
     public void initialize() {
         System.out.println("Initialisation du controlleur..");
         //welcomeText.setText("Welcome to JavaFX Application!");
@@ -99,50 +100,61 @@ public class HelloController {
 
 
     private void handleMouseClick(int row, int col) {
+        Piece piecedejeu = partie.getPlateau().get(row).get(col);
+            if (selectedPiece != null && partie.getPlateau().get(row).get(col) != null && !partie.getPlateau().get(row).get(col).getCouleur().equals(selectedPiece.getCouleur())) {
+                    handleMouseClick_rect(row, col);
 
-        if (selectedPiece != null) {
-            Piece anciennePiece=selectedPiece;
-            selectedPiece= partie.getPlateau().get(row).get(col);
-            handleMouseClick_rect(row,col);
-            //if(!anciennePiece.getCouleur().equals(selectedPiece.getCouleur())){
-              //  handleMouseClick_rect(row,col);
-            //}
+            }
+
+            selectedPiece = partie.getPlateau().get(row).get(col);
+            System.out.println("Ligne cliquée : " + row + ", Colonne : " + col);
+            if (selectedPiece != null) {
+                    if(selectedPiece.getCouleur().equals("Blanc") && partie.isTourdeJeu() || selectedPiece.getCouleur().equals("Noir") && !partie.isTourdeJeu()){
+                        mvt_possible = partie.mvt_possible(selectedPiece);
+                        System.out.println(mvt_possible);
+                    }
+
+
+
+            }
         }
-        selectedPiece= partie.getPlateau().get(row).get(col);
-        System.out.println("Ligne cliquée : " + row + ", Colonne : " + col);
-        if(selectedPiece!=null){
-            mvt_possible = partie.mvt_possible(selectedPiece);
-            System.out.println(mvt_possible);
-        }
-    }
 
 
 
-    public void deplacer_piece(int position_h_depart, int position_v_depart,int position_h_arrive,int position_v_arrive,Piece piece){
+
+    public void deplacer_piece(int position_h_depart, int position_v_depart, int position_h_arrive, int position_v_arrive, Piece piece) {
         System.out.println("deplacer_piece");
-        for(int k=0;k<mvt_possible.size();k++){
-            if(position_h_arrive==mvt_possible.get(k).get(0) && position_v_arrive==mvt_possible.get(k).get(1)){
-                if(partie.getPlateau().get(mvt_possible.get(k).get(0)).get(mvt_possible.get(k).get(1))!=null){
-                    grid.getChildren().remove(partie.getPlateau().get(mvt_possible.get(k).get(0)).get(mvt_possible.get(k).get(1)).getImage());
-                    partie.getPlateau().get(mvt_possible.get(k).get(0)).set(mvt_possible.get(k).get(1),null);
-                    //Probleme l'image se supprime pas mais la piece dans la matrice oui ??? et aussi le programme se contrinue pas apres ??
+        for (int k = 0; k < mvt_possible.size(); k++) {
+            if (position_h_arrive == mvt_possible.get(k).get(0) && position_v_arrive == mvt_possible.get(k).get(1)) {
+                Piece pieceArrivee = partie.getPlateau().get(position_h_arrive).get(position_v_arrive);
+                // Si une pièce est présente à la destination et que c'est une pièce de l'adversaire
+                if (pieceArrivee != null && !piece.getCouleur().equals(pieceArrivee.getCouleur())) {
+                    System.out.println("Capturing piece at: " + position_h_arrive + ", " + position_v_arrive);
+                    grid.getChildren().remove(pieceArrivee.getImage());  // Supprime l'image de la pièce capturée de la grille
+                    partie.getPlateau().get(position_h_arrive).set(position_v_arrive, null);  // Supprime la pièce du plateau
                 }
-                partie.getPlateau().get(mvt_possible.get(k).get(0)).set(mvt_possible.get(k).get(1),piece);
-                piece.setPosition_h(mvt_possible.get(k).get(0));
 
-                piece.setPosition_v(mvt_possible.get(k).get(1));
-                System.out.println(piece.getPosition_h()+" "+piece.getPosition_v());
-                partie.getPlateau().get(position_h_depart).set(position_v_depart,null);
-                System.out.println("aaa");
-                grid.getChildren().remove(selectedPiece.getImage());
-                System.out.println(position_h_arrive+" "+position_v_arrive);
-                piece.getImage().setOnMouseClicked(event -> handleMouseClick(position_h_arrive,position_v_arrive));
-                ImageView imageView= piece.getImage();
+                // Déplacer la pièce vers la nouvelle position
+                partie.getPlateau().get(position_h_arrive).set(position_v_arrive, piece);
+                piece.setPosition_h(position_h_arrive);
+                piece.setPosition_v(position_v_arrive);
+                partie.getPlateau().get(position_h_depart).set(position_v_depart, null);
+
+                // Mettre à jour l'affichage
+                grid.getChildren().remove(piece.getImage());  // Supprime l'image de l'ancienne position
+                piece.getImage().setOnMouseClicked(event -> handleMouseClick(position_h_arrive, position_v_arrive));
+                ImageView imageView = piece.getImage();
                 imageView.setFitHeight(65);
                 imageView.setFitWidth(65);
-                grid.add(piece.getImage(), position_v_arrive, position_h_arrive);
+                grid.add(imageView, position_v_arrive, position_h_arrive);
+                if(partie.isTourdeJeu()){
+                    partie.setTourdeJeu(false);
+                }
+                else{
+                    partie.setTourdeJeu(true);
+                }
+                break;
             }
         }
     }
-
 }
