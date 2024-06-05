bf0fc7f9d7dec674a03e7b40c6b62aedb156a897
BOISDANGHIEN-Evan-2326024e <keyzaks23@gmail.com>
2024-06-05 12:02:33 +0200

    Mouvements completement fini avec tour par tour sur lesm ouvements, a faire echec, echec et mat et timer

--- src/main/java/com/s201_chess/HelloController.java
+++ src/main/java/com/s201_chess/HelloController.java
@@ -101,29 +101,33 @@ public class HelloController {
 
     private void handleMouseClick(int row, int col) {
         Piece piecedejeu = partie.getPlateau().get(row).get(col);
-            if (selectedPiece != null && partie.getPlateau().get(row).get(col) != null && !partie.getPlateau().get(row).get(col).getCouleur().equals(selectedPiece.getCouleur())) {
-                    handleMouseClick_rect(row, col);
 
+        System.out.println(partie.isTourdeJeu());
+
+            if (selectedPiece != null && partie.getPlateau().get(row).get(col) != null && !partie.getPlateau().get(row).get(col).getCouleur().equals(selectedPiece.getCouleur())) {
+                handleMouseClick_rect(row, col);
             }
 
             selectedPiece = partie.getPlateau().get(row).get(col);
             System.out.println("Ligne cliquée : " + row + ", Colonne : " + col);
-            if (selectedPiece != null) {
-                    if(selectedPiece.getCouleur().equals("Blanc") && partie.isTourdeJeu() || selectedPiece.getCouleur().equals("Noir") && !partie.isTourdeJeu()){
-                        mvt_possible = partie.mvt_possible(selectedPiece);
-                        System.out.println(mvt_possible);
-                    }
 
+            if (selectedPiece != null) {
+                if (selectedPiece.getCouleur().equals("Blanc") && partie.isTourdeJeu() || selectedPiece.getCouleur().equals("Noir") && !partie.isTourdeJeu()) {
+                    mvt_possible = partie.mvt_possible(selectedPiece);
+                    System.out.println(mvt_possible);
+                }
 
 
             }
+
+
         }
 
 
 
 
+
     public void deplacer_piece(int position_h_depart, int position_v_depart, int position_h_arrive, int position_v_arrive, Piece piece) {
-        System.out.println("deplacer_piece");
         for (int k = 0; k < mvt_possible.size(); k++) {
             if (position_h_arrive == mvt_possible.get(k).get(0) && position_v_arrive == mvt_possible.get(k).get(1)) {
                 Piece pieceArrivee = partie.getPlateau().get(position_h_arrive).get(position_v_arrive);
@@ -153,6 +157,9 @@ public class HelloController {
                 else{
                     partie.setTourdeJeu(true);
                 }
+                partie.estEchec();
+                System.out.println(partie.isRoiBEchec());
+                System.out.println(partie.isRoiNEchec());
                 break;
             }
         }
