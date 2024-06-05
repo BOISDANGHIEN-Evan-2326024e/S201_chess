ec088a9ed1f7a3d67bf7a6460ec4a0e8412143e4
mathieu <mathieu.gravoille@etu.univ-amu.fr>
2024-06-05 11:07:25 +0200

    juste un commit

--- src/main/java/com/s201_chess/LoginController.java
+++ src/main/java/com/s201_chess/LoginController.java
@@ -1,6 +1,8 @@
 package com.s201_chess;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 //import javafx.scene.control.PasswordField;
@@ -8,7 +10,11 @@ import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 
-public class LoginController extends GridPane {
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class LoginController extends GridPane implements Initializable {
     @FXML
     VBox vb;
     @FXML
@@ -25,7 +31,19 @@ public class LoginController extends GridPane {
     Button cancelButton;
 
     @FXML
-    private void loginClicked() {
+    private void loginClicked() throws IOException {
+        HelloController helloController = new HelloController();
+        helloController.initialize();
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        try {
+            fxmlLoader.getClassLoader();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 
 //    @FXML
