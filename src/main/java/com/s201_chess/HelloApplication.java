6f80bdd5a7d04e1cd4fb88b4b9ac7fe48d6ab73d
BOISDANGHIEN-Evan-2326024e <keyzaks23@gmail.com>
2024-06-06 16:00:00 +0200

    début du css pour une page ressemblante à chess.com

--- src/main/java/com/s201_chess/HelloApplication.java
+++ src/main/java/com/s201_chess/HelloApplication.java
@@ -12,7 +12,11 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+
         Scene scene = new Scene(fxmlLoader.load(), 777, 659);
+        if (scene != null) {
+            scene.getStylesheets().add(getClass().getResource("Login.css").toExternalForm());
+        }
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
